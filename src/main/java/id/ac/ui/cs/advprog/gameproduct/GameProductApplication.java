6c4fac8125789024aa10cc9f101047d428a12d86
rakbidb <rakbidb@gmail.com>
2024-05-26 04:23:20 +0700

    inisialisasi springboot

---
+++ src/main/java/id/ac/ui/cs/advprog/gameproduct/GameProductApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gameproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/gameproduct/GameProductApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gameproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
